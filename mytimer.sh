7c01c184636d0682ebcaaf57ced45a922334924c
Harun Oral <HarunOr@users.noreply.github.com>
2023-11-30 18:39:45 +0100

    update

--- mytimer.sh
+++ mytimer.sh
@@ -3,30 +3,41 @@
 # File to store the start time
 START_TIME_FILE="$HOME/start_time.txt"
 
-reset_timer() {
-    if [ -f "$START_TIME_FILE" ]; then
-        date +%s > "$START_TIME_FILE"
-        echo "Timer reset"
-    else
-        echo "Timer start file does not exist. Cannot reset."
-    fi
-    exit 0
+# Start timer
+start_timer() {
+    date +%s > "$START_TIME_FILE"
+    echo "Timer started"
 }
 
-# Check for 'reset' argument
-if [ "$1" == "reset" ]; then
-    reset_timer
-fi
+# Stop timer
+stop_timer() {
+    if [ -f "$START_TIME_FILE" ]; then
+        rm "$START_TIME_FILE"
+        echo "Timer stopped"
+    else
+        echo "Timer is not running."
+    fi
+}
 
-# Initialize timer if not already set
-if [ ! -f "$START_TIME_FILE" ]; then
-    date +%s > "$START_TIME_FILE"
-fi
-
-# Read the start time from the file
-start_time=$(cat "$START_TIME_FILE")
-current_time=$(date +%s)
-elapsed=$(( current_time - start_time ))
-
-# Format and output the elapsed time in HH:mm:ss
-printf "%02d:%02d:%02d\n" $((elapsed/3600)) $((elapsed%3600/60)) $((elapsed%60))
+# Main script logic
+case "$1" in
+    start)
+        start_timer
+        ;;
+    stop)
+        stop_timer
+        ;;
+    reset)
+        start_timer
+        ;;
+    *)
+        if [ -f "$START_TIME_FILE" ]; then
+            start_time=$(cat "$START_TIME_FILE")
+            current_time=$(date +%s)
+            elapsed=$(( current_time - start_time ))
+            printf "%02d:%02d:%02d\n" $((elapsed/3600)) $((elapsed%3600/60)) $((elapsed%60))
+        else
+            echo "Timer is not running. Use 'mytimer start' to begin."
+        fi
+        ;;
+esac
